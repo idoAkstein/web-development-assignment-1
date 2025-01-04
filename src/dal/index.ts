f99cfdb7b3bcfecdfe7f58bff5d79b16c5be6321
idoakstein1 <idoakstein1@gmail.com>
2025-01-04 16:22:20 +0200

    added update user request

--- src/dal/index.ts
+++ src/dal/index.ts
@@ -1,2 +1,3 @@
 export { createComment, deleteComment, editComment, getCommentByID, getCommentsByPostID } from './comments';
 export { createPost, editPost, getAllPosts, getPostById } from './posts';
+export { createUser, editUser } from './user';
